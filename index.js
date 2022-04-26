9ff5ff53a1160eb0c6aa346d3e295bf8ce071fbe
Ritta <brittanykelly33@gmail.com>
2022-04-25 22:02:53 -0400

    commit tree 3ed5209d6c8ba6ae8ca1ff3884fa5a70185e31f0

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(start) {
+    const street = 42
+   return start >= street ? start - street : street - start
+}
+
+function distanceFromHqInFeet(start) {
+    const blocks = 264
+    return distanceFromHqInBlocks(start)*blocks 
+}
+
+function distanceTravelledInFeet(start,street) {
+    return Math.abs(distanceFromHqInFeet(start) - distanceFromHqInFeet(street))
+}
+
+function calculatesFarePrice(start,destination) {
+    const dinf = distanceTravelledInFeet(start, destination)
+    if (dinf <= 400) {
+        return 0
+    } else if (dinf <= 2000) {
+        return (dinf - 400) * .02
+    } else if (dinf <= 2500) {
+        return 25
+    } else {
+        return "cannot travel that far"
+    }
+}
